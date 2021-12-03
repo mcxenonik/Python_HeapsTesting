0fc25c5897f1beb2a9dadb9ec54e051ad18ec70d
Igor Nowak <i.nowak@stud.elka.pw.edu.pl>
2021-12-03 22:57:05 +0100

    Heap.py added

--- Heap.py
+++ Heap.py
@@ -1,19 +1,20 @@
 class Heap:
-    def __init__(self):
+    def __init__(self, values=None):
         self.heap = []
-        self.heap.append(0)
 
+        if (values is not None):
+            self.naive_create_heap(values)
 
     def _parent(self, index):
-        return (index - 1) / 2
+        return int((index - 1) / 2)
 
 
     def _left(self, index):
-        return 2 * index + 1
+        return int(2 * index + 1)
 
 
     def _right(self, index):
-        return 2 * index + 2
+        return int(2 * index + 2)
 
 
     def _up_heap(self, index):
@@ -41,10 +42,32 @@ class Heap:
 
 
     def delete_element(self):
-        pass
+        root = self.heap[0]                 # ZAPISZ NAJMNIEJSZY ELEMENT
+        self.heap[0] = self.heap[-1]        # WSTAW DO KORZENIA OSTATNI ELEMENT
+        self.heap.pop()                     # USUŃ OSTATNI ELEMENT
+        self._down_heap(0)                  # PRZENIEŚ ELEMENT Z KORZENIA NA WŁAŚCIWE MIEJSCE
+
+        return root                         # ZWRÓĆ NAJMNIEJSZY ELEMENT
 
 
     def add_element(self, value):
         self.heap.append(value)
-        # print("PUSH:", value)
         self._up_heap(len(self.heap) - 1)
+
+
+    def naive_create_heap(self, values):
+        for value in values:
+            self.add_element(value)
+
+
+    def create_heap(self, values):
+        self.heap = values
+        for index in range(int((len(self.heap) - 2) / 2), -1, -1):
+            self._down_heap(index)
+
+
+    def print_heap(self):
+        print(self.heap)
+
+    def print_heap2(self):
+        pass
